b29822359b045298800d2e54a4945031ed6ec0a6
Julian DW <klerpson@gmail.com>
2021-06-11 10:42:18 -0500

    update

--- js/common.js
+++ js/common.js
@@ -137,3 +137,25 @@ $(document).ready(function () {
   });
 
 });
+
+/* =======================
+ // Acordion
+ ======================= */
+
+$(function () {
+  $(".accordion-titulo").click(function (e) {
+
+    e.preventDefault();
+
+    var contenido = $(this).next(".accordion-content");
+
+    if (contenido.css("display") == "none") { //open
+      contenido.slideDown(250);
+      $(this).addClass("open");
+    } else { //close
+      contenido.slideUp(250);
+      $(this).removeClass("open");
+    }
+
+  });
+});
